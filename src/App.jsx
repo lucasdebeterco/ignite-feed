9e185f9ad933ab92d81b08ec006a3e64fd99f943
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2023-10-08 23:48:35 -0300

    creating inital components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import { Header } from './components/header'
+import {Header} from './components/header'
+import styles from './App.module.css'
 import './global.css'
+import {Sidebar} from "./components/Sidebar.jsx";
 
 function App() {
 
-  return (
-    <div>
-      <Header />
-    </div>
-  )
+    return (
+        <div>
+            <Header/>
+
+            <div className={styles.wrapper}>
+                <aside>
+                    <Sidebar />
+                </aside>
+                <main>
+
+                </main>
+            </div>
+        </div>
+    )
 }
 
 export default App
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import styles from './Header.module.css'
+import igniteLogo from './../assets/ignite-logo.svg'
 
 export function Header() {
     return (
         <header className={styles.header}>
-            <h1>Ignite Feed</h1>
+            <img src={igniteLogo} alt='Logo tipo do Ignite' />
         </header>
     )
 }
