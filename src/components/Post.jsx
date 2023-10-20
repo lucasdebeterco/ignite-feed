c58c275fcf8d2c077a37963af36c22dea5d26081
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2023-10-19 23:25:06 -0300

    deleting comments

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -33,6 +33,12 @@ export function Post({ author, publishedAt, content }) {
         setNewCommentText(event.target.value)
     }
 
+    function deleteComment(commentToDelete) {
+        const updatedComments = comments.filter(comment => comment != commentToDelete)
+
+        setComments(updatedComments)
+    }
+
     return (
         <article className={styles.post}>
             <header>
@@ -53,9 +59,9 @@ export function Post({ author, publishedAt, content }) {
             <div className={styles.content}>
                 {content.map(line => {
                     if(line.type === 'paragraph') {
-                        return <p>{line.content}</p>
+                        return <p key={line.content}>{line.content}</p>
                     } else if (line.type === 'link') {
-                        return <p><a href=''>{line.content}</a></p>
+                        return <p key={line.content}><a href=''>{line.content}</a></p>
                     }
                 })}
 
@@ -88,7 +94,11 @@ export function Post({ author, publishedAt, content }) {
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment content={comment} />
+                    return <Comment
+                        key={comment}
+                        content={comment}
+                        onDeleteComment={deleteComment}
+                    />
                 })}
             </div>
         </article>
